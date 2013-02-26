069dae47ed85d183379a0ea7fbe73d3080ec0052
Davoud Taghawi-Nejad <davoud@taghawi-nejad.de>
2013-02-26 16:32:31 +0100

    postprocess.R without trade unification

--- abce/postprocess.R
+++ abce/postprocess.R
@@ -43,39 +43,39 @@ print(table_index_lst)
 rm(con, rs, m, table_index_lst, table_index)
 
 print("Transformations:")
-print("Unify households in trade")
-tt <- trade
-seller_table_index_lst <- matrix(1, nrow=nrow(tt), ncol=1)
-for (i in 1:nrow(tt)) {
-	seller_table_index_lst[i] = i
-	if (unlist(strsplit(tt$seller[i], "_"))[1] == 'firm') {
-		seller_table_index_lst[i] <-tt$seller[i]
-	} else {
-		seller_table_index_lst[i] <- unlist(strsplit(tt$seller[i], "_"))[1]
-	}
-}
+# print("Unify households in trade")
+# tt <- trade
+# seller_table_index_lst <- matrix(1, nrow=nrow(tt), ncol=1)
+# for (i in 1:nrow(tt)) {
+# 	seller_table_index_lst[i] = i
+# 	if (unlist(strsplit(tt$seller[i], "_"))[1] == 'firm') {
+# 		seller_table_index_lst[i] <-tt$seller[i]
+# 	} else {
+# 		seller_table_index_lst[i] <- unlist(strsplit(tt$seller[i], "_"))[1]
+# 	}
+# }
 
-buyer_table_index_lst <- matrix(1, nrow=nrow(tt), ncol=1)
-for (i in 1:nrow(tt)) {
-	buyer_table_index_lst[i] = i
-	if (unlist(strsplit(tt$buyer[i], "_"))[1] == 'firm') {
-		buyer_table_index_lst[i] <- tt$buyer[i]
-	} else {
-		buyer_table_index_lst[i] <- unlist(strsplit(tt$buyer[i], "_"))[1]
-	}
-}
+# buyer_table_index_lst <- matrix(1, nrow=nrow(tt), ncol=1)
+# for (i in 1:nrow(tt)) {
+# 	buyer_table_index_lst[i] = i
+# 	if (unlist(strsplit(tt$buyer[i], "_"))[1] == 'firm') {
+# 		buyer_table_index_lst[i] <- tt$buyer[i]
+# 	} else {
+# 		buyer_table_index_lst[i] <- unlist(strsplit(tt$buyer[i], "_"))[1]
+# 	}
+# }
 
-tt$buyer <- buyer_table_index_lst
-tt$seller <- seller_table_index_lst
+# tt$buyer <- buyer_table_index_lst
+# tt$seller <- seller_table_index_lst
 
-rm(seller_table_index_lst)
-rm(buyer_table_index_lst)
-rm(i)
-print('OK')
-print("Compress 'trade' to 'trade_unified'")
-trade_unified <- ddply(tt, c('round', 'buyer', 'seller','good'), summarize, quantity=sum(quantity), price=mean(price))
-rm(tt)
-print('OK')
+# rm(seller_table_index_lst)
+# rm(buyer_table_index_lst)
+# rm(i)
+# print('OK')
+#print("Compress 'trade' to 'trade_unified'")
+#trade_unified <- ddply(tt, c('round', 'buyer', 'seller','good'), summarize, quantity=sum(quantity), price=mean(price))
+#rm(tt)
+#print('OK')
 #INTERACTIVE
 # a <- 1
 # i <-1
