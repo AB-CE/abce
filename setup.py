be36c1369b0a9536a7e4882b8e0753c85a486d1c
Davoud Taghawi-Nejad <davoud@taghawi-nejad.de>
2014-09-10 12:18:14 +0200

    R completely removed

--- setup.py
+++ setup.py
@@ -16,39 +16,29 @@ setup(
       packages=['abce'],
       modules=['abce_db', 'abcetools', 'postprocess'],
       long_description=open('README.rst').read(),
-      install_requires=['pyparsing==1.5.7', 'numpy','scipy', 'rpy2', 'pyzmq'],
+      install_requires=['pyparsing==1.5.7', 'numpy','scipy', 'pyzmq'],
       include_package_data = True,
      )
 
+print('** **************************************************************************')
+print('**                                                                         **')
+print('** - In the abce-0.X subdirectory you will find  examples ond templates    **')
+print('**                                                                         **')
+print('** - documentation http://davoudtaghawinejad.github.com/abce/              **')
+print('**                                                                         **')
+print('*****************************************************************************')
 if os.name == 'posix':
-    print('** **************************************************************************')
-    print('**                                                                         **')
-    print('** - In the abce-0.X subdirectory you will find  examples ond templates    **')
-    print('**                                                                         **')
-    print('** - documentation http://davoudtaghawinejad.github.com/abce/              **')
-    print('**                                                                         **')
-    print('*****************************************************************************')
     try:
         subprocess.call(["tar", "xf abce-0.3.tar.gz"])
     except:
         print('** Please unzip abce-0.3.tar.gz. There you will find examples, templates and documentation')
         print('** - documentation http://davoudtaghawinejad.github.com/abce/')
-
 elif os.name == 'Darwin':
-    print('************************************************************************************')
-    print('**                                                                                **')
-    print('** - documentation http://davoudtaghawinejad.github.com/abce/                     **')
-    print('**                                                                                **')
-    print('** - please unzip abce-0.X.tar.gz, in there you will find examples and templates  **')
-    print('**                                                                                **')
-    print('************************************************************************************')
+    try:
+        subprocess.call(["tar", "xf abce-0.3.tar.gz"])
+    except:
+        print('** Please unzip abce-0.3.tar.gz. There you will find examples, templates and documentation')
+        print('** - documentation http://davoudtaghawinejad.github.com/abce/')
 else:
-    print('************************************************************************************')
-    print('**                                                                                **')
-    print('** - documentation http://davoudtaghawinejad.github.com/abce/                     **')
-    print('**                                                                                **')
-    print('** - please unzip abce-0.X.tar.gz, in there you will find examples and templates  **')
-    print('**                                                                                **')
-    print('************************************************************************************')
-    print('')
-    print('os.name: %s' % os.name)
+  print('** Please unzip abce-0.3.tar.gz. There you will find examples, templates and documentation')
+  print('** - documentation http://davoudtaghawinejad.github.com/abce/')
