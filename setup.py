898e4a46e99257bbb1f31d1c40b3ecbd9b0ae17e
Davoud Taghawi-Nejad <davoud@taghawi-nejad.de>
2014-09-10 12:18:14 +0200

    R completely removed

--- setup.py
+++ setup.py
@@ -16,27 +16,11 @@ setup(
       packages=['abce'],
       modules=['abce_db', 'abcetools', 'postprocess'],
       long_description=open('README.rst').read(),
-      install_requires=['pyparsing==1.5.7', 'numpy','scipy', 'rpy2', 'pyzmq'],
-      data_files=[('abce', ['abce/postprocess.R'])],
+      install_requires=['pyparsing==1.5.7', 'numpy','scipy', 'pyzmq', 'matplotlib'],
       include_package_data = True,
      )
 
 if os.name == 'posix':
-    try:
-        subprocess.call(["sudo apt-get", "install r-base"])
-    except:
-        print('** *************************************************************************')
-        print('**                                                                        **')
-        print('**  please download and install R from http://cran.r-project.org          **')
-        print('**                                                                        **')
-        print('****************************************************************************')
-    print('** **************************************************************************')
-    print('**                                                                         **')
-    print('** - In the abce-0.X subdirectory you will find  examples ond templates    **')
-    print('**                                                                         **')
-    print('** - documentation http://davoudtaghawinejad.github.com/abce/              **')
-    print('**                                                                         **')
-    print('*****************************************************************************')
     try:
         subprocess.call(["tar", "xf abce-0.3.tar.gz"])
     except:
@@ -44,24 +28,11 @@ if os.name == 'posix':
         print('** - documentation http://davoudtaghawinejad.github.com/abce/')
 
 elif os.name == 'Darwin':
-    print('************************************************************************************')
-    print('**                                                                                **')
-    print('** - please download and install R from http://cran.r-project.org/bin/macosx/     **')
-    print('**                                                                                **')
-    print('** - documentation http://davoudtaghawinejad.github.com/abce/                     **')
-    print('**                                                                                **')
-    print('** - please unzip abce-0.X.tar.gz, in there you will find examples and templates  **')
-    print('**                                                                                **')
-    print('************************************************************************************')
+    try:
+        subprocess.call(["tar", "xf abce-0.3.tar.gz"])
+    except:
+        print('** Please unzip abce-0.3.tar.gz. There you will find examples, templates and documentation')
+        print('** - documentation http://davoudtaghawinejad.github.com/abce/')
 else:
-    print('************************************************************************************')
-    print('**                                                                                **')
-    print('** - pls download and install R from http://cran.r-project.org/bin/windows/base/  **')
-    print('**                                                                                **')
-    print('** - documentation http://davoudtaghawinejad.github.com/abce/                     **')
-    print('**                                                                                **')
-    print('** - please unzip abce-0.X.tar.gz, in there you will find examples and templates  **')
-    print('**                                                                                **')
-    print('************************************************************************************')
-    print('')
-    print('os.name: %s' % os.name)
+  print('** Please unzip abce-0.3.tar.gz. There you will find examples, templates and documentation')
+  print('** - documentation http://davoudtaghawinejad.github.com/abce/')
