3468069c43456dc8f3a37039bc957394389180d8
davoud taghawi-nejad <davoud@taghawi-nejad.de>
2015-09-29 10:19:16 -0300

    conf.py in docs for read the docs

--- doc/conf.py
+++ doc/conf.py
@@ -12,7 +12,18 @@
 # serve to show the default.
 
 import sys, os
+try:
+    from unittest.mock import MagicMock
 
+    class Mock(MagicMock):
+        @classmethod
+        def __getattr__(cls, name):
+                return Mock()
+
+    MOCK_MODULES = ['pygtk', 'gtk', 'gobject', 'argparse', 'numpy', 'pandas']
+    sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
+except ImportError:
+    pass
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
