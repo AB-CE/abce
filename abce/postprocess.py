f7619315b866b7137c3722b384297e65f8f7e43a
davoud taghawi-nejad <davoud@taghawi-nejad.de>
2015-10-18 20:34:28 -0200

    .csv files have an named index column

--- abce/postprocess.py
+++ abce/postprocess.py
@@ -24,14 +24,14 @@ def to_csv(directory):
             print()
             print("If you keep having problems email davoudtaghawinejad@gmail.com")
             raise
-        table.to_csv(table_name + '.csv')
+        table.to_csv(table_name + '.csv', index_label='index')
         if u'id' in table.columns:
             del table['id']
             grouped = table.groupby('round')
             aggregated = grouped.sum()
-            aggregated.to_csv(table_name + '_aggregate.csv')
+            aggregated.to_csv(table_name + '_aggregate.csv', index_label='index')
             try:
                 meaned = grouped.mean()
-                meaned.to_csv(table_name + '_mean.csv')
+                meaned.to_csv(table_name + '_mean.csv', index_label='index')
             except pd.core.groupby.DataError:
                 pass
