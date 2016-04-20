9adf85b016f1c638d3b2e52c923381475ea70332
davoud taghawi-nejad <davoud@taghawi-nejad.de>
2016-04-20 17:06:22 -0300

    setup compiles cython when possible

--- setup.py
+++ setup.py
@@ -9,13 +9,22 @@ except ImportError:
     from distutils.extension import Extension
 
 
-cmdclass = {}
-ext_modules = []
+cmdclass = { }
+ext_modules = [ ]
+
+try:
+    from Cython.Distutils import build_ext
+    ext_modules += [
+        Extension("abce.trade", [ "abce/trade.pyx" ]),
+    ]
+    cmdclass.update({ 'build_ext': build_ext })
+except ImportError:
+    ext_modules += [
+        Extension("abce.trade", [ "abce/trade.c" ]),
+    ]
 
 
-ext_modules += [
-Extension("abce.trade", [ "abce/trade.c" ]),
-]
+
 
 
 
@@ -30,31 +39,7 @@ setup(name='abce',
       long_description=open('README.rst').read(),
       install_requires=['pygal', 'numpy', 'pandas', 'networkx', 'flask'],
       include_package_data=True,
-      ext_modules=ext_modules)
+      ext_modules=ext_modules,
+      cmdclass=cmdclass)
 
-print('** **************************************************************************')
-print('**                                                                         **')
-print('** - To use ABCE download templates and examples from                      **')
-print('**                                                                         **')
-print('**    github.com/DavoudTaghawiNejad/abce                                   **')
-print('**    or                                                                    **')
-print('**    https://github.com/DavoudTaghawiNejad/abce/archive/master.zip        **')
-print('**                                                                         **')
-print('** - documentation http://davoudtaghawinejad.github.com/abce/              **')
-print('**                                                                         **')
-print('*****************************************************************************')
-if os.name == 'posix':
-    try:
-        subprocess.call(["tar", "xf abce-0.3.tar.gz"])
-    except:
-        print('** Please unzip abce-0.3.tar.gz. There you will find examples, templates and documentation')
-        print('** - documentation http://davoudtaghawinejad.github.com/abce/')
-elif os.name == 'Darwin':
-    try:
-        subprocess.call(["tar", "xf abce-0.3.tar.gz"])
-    except:
-        print('** Please unzip abce-0.3.tar.gz. There you will find examples, templates and documentation')
-        print('** - documentation http://davoudtaghawinejad.github.com/abce/')
-else:
-  print('** Please unzip abce-0.3.tar.gz. There you will find examples, templates and documentation')
-  print('** - documentation http://davoudtaghawinejad.github.com/abce/')
+
