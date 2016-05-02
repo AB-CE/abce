efbc46ee090d8490f791aa346ab2ca0bf3a3a7d2
Davoud Taghawi-Nejad <davoud@taghawi-nejad.de>
2016-05-02 14:35:52 -0300

    example create_agents, with test

---
+++ example/create_agents/household.py
@@ -0,0 +1,18 @@
+from __future__ import division
+import abce
+from abce import NotEnoughGoods
+
+
+class Household(abce.Agent, abce.Household):
+    def init(self, simulation_parameters, agent_parameters):
+        # your agent initialization goes here, not in __init__
+        self.count = 1
+
+    def print_id(self):
+        #print(self.group, self.id)
+        pass
+
+    def receive_message(self):
+        messages = self.get_messages('msg')
+        assert len(messages) == 1, self.id
+        assert messages[0].content == self.id, (self.id, messages[0].content)
---
+++ example/create_agents/messenger.py
@@ -0,0 +1,19 @@
+from __future__ import division
+import abce
+from abce import NotEnoughGoods
+
+
+class Messenger(abce.Agent):
+    def init(self, simulation_parameters, agent_parameters):
+        # your agent initialization goes here, not in __init__
+        self.count = 1
+
+    def messaging(self):
+        max_firm = 2 ** self.round
+        for id in range(max_firm):
+            self.message('firm', id, 'msg', id)
+
+        max_hh = 2 ** self.round
+        print max_hh
+        for id in range(max_hh):
+            self.message('household', id, 'msg', id)
