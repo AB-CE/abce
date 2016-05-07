0d66961e09c44d7bc1347187bdce56f5e67c882d
Davoud Taghawi-Nejad <taghawi@Davouds-MacBook-Pro.local>
2016-05-06 23:18:36 -0300

    one_household_one_firm_with_logic

---
+++ example/one_household_one_firm_with_logic/firm.py
@@ -0,0 +1,41 @@
+from __future__ import division  # makes division work correctly
+import abce
+import random
+
+class Firm(abce.Agent, abce.Firm):
+    def init(self, simulation_parameters, agent_parameters):
+        """ 1. Gets an initial amount of money
+        2. create a cobb_douglas function: GOOD = 1 * labor ** 1.
+        """
+        self.create('money', 1000)
+        self.mygood = "GOOD%i" % self.id  # GOOD1 if self.id == 1
+        self.set_cobb_douglas(self.mygood, 1, {"labor": 1})
+        self.price = random.random() * 2
+
+
+    def buy_labor(self):
+        """ receives all labor offers and accepts them one by one """
+        oo = self.get_offers("labor")
+        for offer in oo:
+            self.accept(offer, min(offer.quantity, self.possession('money')))
+
+    def production(self):
+        """ uses all labor that is available and produces
+        according to the set cobb_douglas function """
+        self.produce_use_everything()
+
+    def quotes(self):
+        self.message('household', 0, 'quote', (self.mygood, self.price))
+
+    def sell_goods(self):
+        """ offers one unit of labor to firm 0, for the price of 1 "money" """
+        oo = self.get_offers(self.mygood)
+        for offer in oo:
+            self.accept(offer, min(offer.quantity, self.possession(self.mygood)))
+
+    def adjust_price(self):
+        self.inventory = self.possession(self.mygood)
+        if self.inventory < 4:
+            self.price += random.random() * 0.01  # random number [0, 0.1]
+        if self.inventory > 6:
+            self.price = max(0.01, self.price - random.random() * 0.01)  # random number [0, 0.1]
---
+++ example/one_household_one_firm_with_logic/start.py
@@ -0,0 +1,50 @@
+""" 1. declared the timeline
+    2. build one Household and one Firm follow_agent
+    3. For every labor_endowment an agent has he gets one trade or usable labor
+    per round. If it is not used at the end of the round it disapears.
+    4. Firms' and Households' possesions are monitored ot the points marked in
+    timeline.
+"""
+
+from __future__ import division
+from abce import Simulation, gui
+from firm import Firm
+from household import Household
+
+parameters = {'name': '2x2',
+              'random_seed': None,
+              'rounds': 2500,
+              'num_firms': 10}
+
+#@gui(parameters)
+def main(parameters):
+    simulation = Simulation(rounds=parameters['rounds'], cores=1)
+    action_list = [
+        ('household', 'sell_labor'),
+        ('firm', 'buy_labor'),
+        ('firm', 'production'),
+        ('firm', 'panel'),
+        ('firm', 'quotes'),
+        ('household', 'buy_goods'),
+        ('firm', 'sell_goods'),
+        ('household', 'aggregate'),
+        ('household', 'consumption'),
+        ('firm', 'adjust_price')]
+    simulation.add_action_list(action_list)
+
+    simulation.declare_round_endowment(resource='adult', units=1, product='labor')
+    simulation.declare_perishable(good='labor')
+
+    simulation.aggregate('household', possessions=['money', 'GOOD'],
+                         variables=['current_utiliy'])
+    simulation.panel('firm', possessions=['money', 'GOOD'],
+                    variables=['price', 'inventory'])
+
+    simulation.build_agents(Firm, 'firm', number=parameters['num_firms'])
+    simulation.build_agents(Household, 'household', number=1, parameters=parameters)
+
+    simulation.run()
+    simulation.graphs()
+
+if __name__ == '__main__':
+    main(parameters)
