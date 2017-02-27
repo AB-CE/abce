51ccd58113bf19a1bc236c4be8ae850d3cad77cc
davoud taghawi-nejad <davoud@taghawi-nejad.de>
2017-02-27 17:34:22 +0000

    only setuptools

--- setup.py
+++ setup.py
@@ -1,10 +1,6 @@
 #!/usr/bin/env python
-try:
-    from setuptools import setup
-    from setuptools import Extension
-except ImportError:
-    from distutils.core import setup
-    from distutils.extension import Extension
+from setuptools import setup
+from setuptools import Extension
 
 
 cmdclass = { }
