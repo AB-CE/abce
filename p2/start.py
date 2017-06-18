09907fd18b54d2eac1ec48931be4737cff01f08a
rht <rhtbot@protonmail.com>
2017-06-18 21:43:28 +0200

    Further autopep8 on p2 and template

--- p2/start.py
+++ p2/start.py
@@ -6,20 +6,21 @@ from abce import Simulation
 
 def main():
     parameters = {
-    'name': 'name',
-    'rounds': 300
+        'name': 'name',
+        'rounds': 300
     }
 
     s = Simulation(rounds=parameters['rounds'], cores=8)
-    action_list = [#(('myagent', 'youragent'), 'compute'),
-                   ('youragent', 's'),
-                   ('myagent', 'g')]
+    action_list = [  # (('myagent', 'youragent'), 'compute'),
+        ('youragent', 's'),
+        ('myagent', 'g')]
     s.add_action_list(action_list)
 
     s.build_agents(MyAgent, 50000)
     s.build_agents(YourAgent, 50000)
 
     s.run()
+
 
 if __name__ == '__main__':
     main()
