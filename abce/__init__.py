8f4293220384f123085a4ca2d18074bdb442662d
rht <rhtbot@protonmail.com>
2017-06-19 06:14:36 +0200

    Reorg agents template classes

--- abce/__init__.py
+++ abce/__init__.py
@@ -48,27 +48,25 @@ import csv
 import datetime
 import os
 import time
+import random
 import multiprocessing as mp
 from multiprocessing.managers import BaseManager
 import abce.db
 import abce.abcelogger
 from . import postprocess
 from glob import glob
-from .firmmultitechnologies import *
-from .household import Household
 from .agent import *
 from .group import Group
 from collections import defaultdict, OrderedDict
-from .firm import Firm
+from abce.notenoughgoods import NotEnoughGoods
+from abce.agents import (FirmMultiTechnologies, ProductionFunction, Firm, Household,
+                         Utility_Function, SilentDeadAgent, LoudDeadAgent)
 from .quote import Quote
 from .contracting import Contracting
 import json
 from . import abcegui
 from .processorgroup import ProcessorGroup
 from .abcegui import gui
-import random
-from abce.notenoughgoods import NotEnoughGoods
-from abce.deadagent import SilentDeadAgent, LoudDeadAgent
 import numpy as np
 
 
