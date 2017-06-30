cdddc0ff306a927c24873f03efe35f527000420e
Davoud Taghawi-Nejad <davoud@taghawi-nejad.de>
2017-06-30 14:02:38 +0100

    no multithreading unittest on windows

--- unittest/start.py
+++ unittest/start.py
@@ -1,12 +1,15 @@
 import start_core_engine
 import start_logging_test
+import platform
 
 
 start_core_engine.main(processes=1, rounds=50)
 print('Iteration with 1 core finished')
-start_core_engine.main(processes=4, rounds=50)
-print('Iteration with multiple processes finished')
+if platform.system() != 'Windows':
+    start_core_engine.main(processes=4, rounds=50)
+    print('Iteration with multiple processes finished')
 start_logging_test.main(processes=1)
 print('Iteration of logger testing with 1 core finished')
-start_logging_test.main(processes=4)
-print('Iteration of logger testing with multiple processes finished')
+if platform.system() != 'Windows':
+    start_logging_test.main(processes=4)
+    print('Iteration of logger testing with multiple processes finished')
