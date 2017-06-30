7f0b787745750554777be27c202223976aced126
Davoud Taghawi-Nejad <davoud@taghawi-nejad.de>
2017-06-30 19:04:32 +0100

    Put ifmain in unittest start.py

--- unittest/start.py
+++ unittest/start.py
@@ -2,14 +2,14 @@ import start_core_engine
 import start_logging_test
 import platform
 
-
-start_core_engine.main(processes=1, rounds=50)
-print('Iteration with 1 core finished')
-if platform.system() != 'Windows':
-    start_core_engine.main(processes=4, rounds=50)
-    print('Iteration with multiple processes finished')
-start_logging_test.main(processes=1)
-print('Iteration of logger testing with 1 core finished')
-if platform.system() != 'Windows':
-    start_logging_test.main(processes=4)
-    print('Iteration of logger testing with multiple processes finished')
+if __name__ == '__main__':
+    start_core_engine.main(processes=1, rounds=50)
+    print('Iteration with 1 core finished')
+    if platform.system() != 'Windows':
+        start_core_engine.main(processes=4, rounds=50)
+        print('Iteration with multiple processes finished')
+    start_logging_test.main(processes=1)
+    print('Iteration of logger testing with 1 core finished')
+    if platform.system() != 'Windows':
+        start_logging_test.main(processes=4)
+        print('Iteration of logger testing with multiple processes finished')
