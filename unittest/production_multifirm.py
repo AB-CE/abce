a118520d0bc056196382009a14708451d75a1233
Davoud Taghawi-Nejad <davoud@taghawi-nejad.de>
2017-06-30 13:23:08 +0100

    approximate equal in production_multifirm.py unittest

--- unittest/production_multifirm.py
+++ unittest/production_multifirm.py
@@ -138,8 +138,9 @@ class ProductionMultifirm(abce.Agent, abce.FirmMultiTechnologies):
         assert self.possession('e') == 0, self.possession('e')
         expected = 2 * (0.2 * 1 ** 0.5 + 0.2 * 2 ** 0.5 + 0.2 *
                         2 ** 0.5 + 0.2 * 2 ** 0.5 + 0.2 * 2 ** 0.5) ** (1 / 0.5)
-        assert self.possession('consumption_good') == expected, (self.possession(
-            'consumption_good'), expected)
+        assert iszero(self.possession('consumption_good') - expected, (self.possession(
+            'consumption_good'), expected))
+        # this is exactly zero on my computer, but not on travis
         self.destroy('a', 1)
         self.destroy('consumption_good', expected)
 
