4b09d64ab1e4edb90a11a5f884c43dee96d1fdb8
DavoudTaghawiNejad <davoud@taghawi-nejad.de>
2017-08-02 13:59:30 -0300

    remove template as it is moved to abce_examples

--- template/firm.py
+++
@@ -1,9 +0,0 @@
-from __future__ import division
-import abce
-
-
-class Firm(abce.Agent, abce.Firm, abce.Trade):
-    def init(self, simulation_parameters, agent_parameters):
-        # your agent initialization goes here, not in __init__
-        self.num_households = simulation_parameters['households']
-        self.num_firms = simulation_parameters['firms']
