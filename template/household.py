f55607d9030ae5c14efd160f88d9f0a566a53e04
DavoudTaghawiNejad <davoud@taghawi-nejad.de>
2017-08-02 13:59:30 -0300

    Remove template since it has been moved to examples repo

--- template/household.py
+++
@@ -1,8 +0,0 @@
-from __future__ import division
-import abce
-
-
-class Household(abce.Agent, abce.Household, abce.Trade):
-    def init(self, simulation_parameters, agent_parameters):
-        # your agent initialization goes here, not in __init__
-        pass
