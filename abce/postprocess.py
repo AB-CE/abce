9261002b1c58ada89143f269ec58bb1722fa4831
DavoudTaghawiNejad <davoud@taghawi-nejad.de>
2017-08-20 12:27:02 -0300

    Concatinates the tables by agent type

--- abce/postprocess.py
+++ abce/postprocess.py
@@ -1,6 +1,7 @@
 import os
 import sqlite3
 import pandas as pd
+from collections import defaultdict
 
 
 def to_csv(directory):
@@ -9,41 +10,54 @@ def to_csv(directory):
     cursor = db.cursor()
     cursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
     tables = cursor.fetchall()
+    panel = defaultdict(pd.DataFrame)
+    aggs = defaultdict(pd.DataFrame)
     for table_name in tables:
         table_name = table_name[0]
+        table = pd.read_sql_query("SELECT * from %s" % table_name, db)
+
+        typ = table_name.split('___')[0]
+        group = table_name.split('___')[1]
+        print(typ, group, table.columns)
+        if typ == 'panel':
+            panel[group] = pd.concat([panel[group], table], axis=1)
+        elif typ == 'aggregate':
+            aggs[group] = pd.concat([aggs[group], table], axis=1)
+        else:
+            raise Exception(typ)
+
+    for group, df in aggs.items():
+        df = df.loc[:, ~df.columns.duplicated()]
+        df.to_csv('aggregate_' + group + '.csv', index_label='round')
+
+
+    for group, df in panel.items():
+        df = df.loc[:, ~df.columns.duplicated()]
+        df.to_csv(group + '.csv', index=False)
+
+        if u'id' in df:
+            del df['id']
+        grouped = df.groupby('round')
+        aggregated = grouped.sum()
+        aggregated.rename(
+            columns={col: col + '_ttl'
+                     for col in aggregated.columns}, inplace=True)
         try:
-            table = pd.read_sql_query("SELECT * from %s" % table_name, db)
-        except:
-            print("your pip installation of pandas is not up to date.")
-            print("If you keep having problems email ")
-            print("davoudtaghawinejad@gmail.com")
-            raise
-        table.to_csv(table_name + '.csv', index=False)
+            meaned = grouped.mean()
+            meaned.rename(
+                columns={col: col + '_mean'
+                         for col in meaned.columns}, inplace=True)
+        except pd.core.groupby.DataError:
+            meaned = pd.DataFrame()
+        try:
+            std = grouped.std()
+            std.rename(
+                columns={col: col + '_std'
+                         for col in std.columns}, inplace=True)
+        except pd.core.groupby.DataError:
+            std = pd.DataFrame()
 
-        if u'id' in table.columns:
-            del table['id']
-            grouped = table.groupby('round')
-            aggregated = grouped.sum()
-            aggregated.rename(
-                columns={col: col + '_ttl'
-                         for col in aggregated.columns}, inplace=True)
-            try:
-                meaned = grouped.mean()
-                meaned.rename(
-                    columns={col: col + '_mean'
-                             for col in meaned.columns}, inplace=True)
-            except pd.core.groupby.DataError:
-                meaned = pd.DataFrame()
-            try:
-                std = grouped.std()
-                std.rename(
-                    columns={col: col + '_std'
-                             for col in std.columns}, inplace=True)
-            except pd.core.groupby.DataError:
-                std = pd.DataFrame()
-
-            result = pd.concat([aggregated, meaned, std], axis=1)
-            result.to_csv('aggregate_' + table_name + '.csv',
-                          index_label='round', date_format='%Y-%m-%d')
-
+        result = pd.concat([aggregated, meaned, std], axis=1)
+        result.to_csv('aggregated_' + group + '.csv',
+                      index_label='round')
     os.chdir('../..')
