50db927118fc2d69bff5996198f065de5a4d23de
DavoudTaghawiNejad <davoud@taghawi-nejad.de>
2017-08-20 12:33:34 -0300

    Removes unused funtions in db.py

--- abce/db.py
+++ abce/db.py
@@ -39,13 +39,6 @@ class Database(threading.Thread):
                               "(round INT, good VARCHAR(50), seller VARCHAR(50), buyer VARCHAR(50), price FLOAT, quantity FLOAT)")
         return 'INSERT INTO trade (round, good, seller, buyer, price, quantity) VALUES (%i, "%s", "%s", "%s", "%s", %f)'
 
-    def add_log(self, table_name):
-        self.database.execute("CREATE TABLE log_" + table_name +
-                              "(round INT, id INT, PRIMARY KEY(round, id))")
-
-    def add_panel(self, group, column_names):
-        self.panels['panel_' + group] = list(column_names)
-
     def run(self):
         self.dataset_db = dataset.connect('sqlite:///' + self.directory + '/dataset.db')
         self.dataset_db.query('PRAGMA synchronous=OFF')
@@ -67,18 +60,6 @@ class Database(threading.Thread):
 
         if self.trade_log:
             trade_ex_str = self.add_trade_log()
-        for table_name in self.panels:
-            panel_str = ' FLOAT,'.join(self.panels[table_name]) + ' FLOAT,'
-
-            create_str = "CREATE TABLE " + table_name + "(id INT, round INT, %s PRIMARY KEY(round, id))" % panel_str
-            self.database.execute(create_str)
-
-            format_strings = ','.join(['?'] * (2 + len(self.panels[table_name])))
-            self.ex_str[table_name] = "INSERT INTO " + table_name + \
-                "(id, round, " + ','.join(list(self.panels[table_name])) + ") VALUES (%s)" % format_strings
-
-            create_str = "CREATE TABLE " + table_name + "(round INT, %s PRIMARY KEY(round))" % agg_str
-            self.database.execute(create_str)
 
         while True:
             try:
@@ -143,74 +124,6 @@ class Database(threading.Thread):
                     'aggregate___%s' % group, primary_id='index')
                 self.table_aggregates[group].insert(result)
             self.aggregation[group].clear()
-
-    def write_or_update(self, table_name, data_to_write):
-        insert_str = "INSERT OR IGNORE INTO " + table_name + \
-            "(" + ','.join(list(data_to_write.keys())) + ") VALUES (%s);"
-        update_str = "UPDATE " + table_name + \
-            " SET %s  WHERE CHANGES()=0 and round=%s and id=%s;"
-        update_str = update_str % (','.join('%s=?' % key for key in data_to_write),
-                                   data_to_write['round'], data_to_write['id'])
-        rows_to_write = list(data_to_write.values())
-        format_strings = ','.join(['?'] * len(rows_to_write))
-        try:
-            self.database.execute(insert_str % format_strings, rows_to_write)
-        except sqlite3.OperationalError as e:
-            errormsg = str(e)
-            if 'no such table' in errormsg:
-                raise TableMissing(table_name)
-            if not('has no column named' in errormsg):
-                raise
-            self.new_column(table_name, data_to_write)
-            self.write_or_update(table_name, data_to_write)
-        self.database.execute(update_str, rows_to_write)
-
-    def write_pa(self, table_name, rows_to_write):
-        self.database.execute(self.ex_str[table_name], rows_to_write)
-
-    def write(self, table_name, data_to_write):
-        try:
-            ex_str = "INSERT INTO " + table_name + \
-                "(" + ','.join(list(data_to_write.keys())) + ") VALUES (%s)"
-        except TypeError:
-            raise TypeError("good names must be strings",
-                            list(data_to_write.keys()))
-        rows_to_write = list(data_to_write.values())
-        format_strings = ','.join(['?'] * len(rows_to_write))
-        try:
-            self.database.execute(ex_str % format_strings, rows_to_write)
-        except sqlite3.OperationalError as e:
-            errormsg = str(e)
-            if 'no such table' in errormsg:
-                raise TableMissing(table_name)
-            if not('has no column named' in errormsg):
-                raise
-            self.new_column(table_name, data_to_write)
-            self.write(table_name, data_to_write)
-        except sqlite3.InterfaceError:
-            print((ex_str % format_strings, rows_to_write))
-            raise
-
-    def new_column(self, table_name, data_to_write):
-        rows_to_write = list(data_to_write.values())
-        self.database.execute("""PRAGMA table_info(""" + table_name + """)""")
-        existing_columns = [row[1] for row in self.database]
-        new_columns = set(data_to_write.keys()).difference(existing_columns)
-        for column in new_columns:
-            try:
-                if is_convertable_to_float(data_to_write[column]):
-                    self.database.execute(
-                        """ ALTER TABLE """ + table_name + """ ADD """ + column + """ FLOAT;""")
-                else:
-                    self.database.execute(
-                        """ ALTER TABLE """ + table_name + """ ADD """ + column + """ VARCHAR(50);""")
-            except TypeError:
-                rows_to_write.remove(data_to_write[column])
-                del data_to_write[column]
-
-    def aggregate(self, table_name, data):
-        for key in data:
-            self.data[table_name][key].append(data[key])
 
 
 class TableMissing(sqlite3.OperationalError):
