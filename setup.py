ecd680c54a6ac0058153d94e90183be0da26a995
DavoudTaghawiNejad <davoud@taghawi-nejad.de>
2017-08-30 16:59:28 -0300

    Installs cython only when run with CPython

--- setup.py
+++ setup.py
@@ -1,29 +1,39 @@
 #!/usr/bin/env python
-try:
-    from setuptools import setup
-    from setuptools import Extension
-except ImportError:
-    from distutils.core import setup
-    from distutils.extension import Extension
+import platform
 
+from setuptools import setup
+from setuptools import Extension
 
-cmdclass = {}
-ext_modules = []
+print(platform.python_implementation())
+if platform.python_implementation() == 'CPython':
 
-try:
-    from Cython.Distutils import build_ext
-    ext_modules += [
-        Extension("abce.trade", ["abce/trade.pyx"]),
-        Extension("abce.multicurrencytrade", ["abce/multicurrencytrade.pyx"]),
-        Extension("abce.online_variance", ["abce/online_variance.pyx"]),
-    ]
-    cmdclass.update({'build_ext': build_ext})
-except ImportError:
-    ext_modules += [
-        Extension("abce.trade", ["abce/trade.c"]),
-        Extension("abce.multicurrencytrade", ["abce/multicurrencytrade.c"]),
-        Extension("abce.online_variance", ["abce/online_variance.c"]),
-    ]
+    cmdclass = {}
+    ext_modules = []
+
+    try:
+        from Cython.Distutils import build_ext
+        ext_modules += [
+            Extension("abce.trade", ["abce/trade.pyx"]),
+            Extension("abce.multicurrencytrade",
+                      ["abce/multicurrencytrade.pyx"]),
+            Extension("abce.online_variance", ["abce/online_variance.pyx"]),
+            Extension("abce.online_variance", ["abce/online_variance.pxd"]),
+        ]
+        cmdclass.update({'build_ext': build_ext})
+    except ImportError:
+        ext_modules += [
+            Extension("abce.trade", ["abce/trade.c"]),
+            Extension("abce.multicurrencytrade",
+                      ["abce/multicurrencytrade.c"]),
+            Extension("abce.online_variance", ["abce/online_variance.c"]),
+        ]
+    cython_specific = ['numpy >= 1.10.2p',
+                       'pandas >= 0.17.1',
+                       'bokeh == 0.12.6']
+else:
+    ext_modules = []
+    cmdclass = {}
+    cython_specific = []
 
 
 setup(name='abce',
@@ -35,12 +45,9 @@ setup(name='abce',
       package_dir={'abce': 'abce'},
       packages=['abce'],
       long_description=open('README.rst').read(),
-      install_requires=['numpy >= 1.10.2p',
-                        'pandas >= 0.17.1',
-                        'networkx >= 1.9.1',
+      install_requires=['networkx >= 1.9.1',
                         'flexx >= 0.4.1',
-                        'bokeh == 0.12.6',
-                        'future'],
+                        'future'] + cython_specific,
       include_package_data=True,
       ext_modules=ext_modules,
       use_2to3=True,
