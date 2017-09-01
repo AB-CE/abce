3045ac34259a28944239b779fed29474a57fadc3
DavoudTaghawiNejad <davoud@taghawi-nejad.de>
2017-09-01 17:44:00 -0300

    no cython in windows

--- setup.py
+++ setup.py
@@ -4,36 +4,38 @@ import platform
 from setuptools import setup
 from setuptools import Extension
 
-print(platform.python_implementation())
+
+cmdclass = {}
+ext_modules = []
+cpython_specific = []
+
+
 if platform.python_implementation() == 'CPython':
+    cpython_specific = ['numpy >= 1.10.2p',
+                        'pandas >= 0.17.1',
+                        'bokeh == 0.12.7']
 
-    cmdclass = {}
-    ext_modules = []
-
-    try:
-        from Cython.Distutils import build_ext
-        ext_modules += [
-            Extension("abce.trade", ["abce/trade.pyx"]),
-            Extension("abce.multicurrencytrade",
-                      ["abce/multicurrencytrade.pyx"]),
-            Extension("abce.online_variance", ["abce/online_variance.pyx"]),
-            Extension("abce.online_variance", ["abce/online_variance.pxd"]),
-        ]
-        cmdclass.update({'build_ext': build_ext})
-    except ImportError:
-        ext_modules += [
-            Extension("abce.trade", ["abce/trade.c"]),
-            Extension("abce.multicurrencytrade",
-                      ["abce/multicurrencytrade.c"]),
-            Extension("abce.online_variance", ["abce/online_variance.c"]),
-        ]
-    cython_specific = ['numpy >= 1.10.2p',
-                       'pandas >= 0.17.1',
-                       'bokeh == 0.12.7']
-else:
-    ext_modules = []
-    cmdclass = {}
-    cython_specific = []
+    if platform.system() != 'Windows':
+        try:
+            from Cython.Distutils import build_ext
+            ext_modules += [
+                Extension("abce.trade", ["abce/trade.pyx"]),
+                Extension("abce.multicurrencytrade",
+                          ["abce/multicurrencytrade.pyx"]),
+                Extension("abce.online_variance",
+                          ["abce/online_variance.pyx"]),
+                Extension("abce.online_variance",
+                          ["abce/online_variance.pxd"]),
+            ]
+            cmdclass.update({'build_ext': build_ext})
+        except ImportError:
+            ext_modules += [
+                Extension("abce.trade", ["abce/trade.c"]),
+                Extension("abce.multicurrencytrade",
+                          ["abce/multicurrencytrade.c"]),
+                Extension("abce.online_variance",
+                          ["abce/online_variance.c"]),
+            ]
 
 
 setup(name='abce',
@@ -47,7 +49,7 @@ setup(name='abce',
       long_description=open('README.rst').read(),
       install_requires=['networkx >= 1.9.1',
                         'flexx >= 0.4.1',
-                        'future'] + cython_specific,
+                        'future'] + cpython_specific,
       include_package_data=True,
       ext_modules=ext_modules,
       use_2to3=True,
