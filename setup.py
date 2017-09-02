e50c9a0b924a4b72934feefa9db0b9d64cddc2d8
DavoudTaghawiNejad <davoud@taghawi-nejad.de>
2017-09-02 12:12:33 -0300

    Fixes setup.py to support pypy and package installation

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ try:
 except ImportError:
     from distutils.core import setup
     from distutils.extension import Extension
+import platform
 
 
 cmdclass = {}
@@ -24,24 +25,48 @@ except ImportError:
         Extension("abce.multicurrencytrade", ["abce/multicurrencytrade.c"]),
         Extension("abce.online_variance", ["abce/online_variance.c"]),
     ]
-
-
-setup(name='abce',
-      version='0.7.0a0',
-      author='Davoud Taghawi-Nejad',
-      author_email='Davoud@Taghawi-Nejad.de',
-      description='Agent-Based Complete Economy modelling platform',
-      url='https://github.com/DavoudTaghawiNejad/abce.git',
-      package_dir={'abce': 'abce'},
-      packages=['abce'],
-      long_description=open('README.rst').read(),
-      install_requires=['numpy >= 1.10.2p',
+if platform.python_implementation() == 'CPython':
+    install_requires = ['numpy >= 1.10.2p',
                         'pandas >= 0.17.1',
+                        'bokeh == 0.12.7',
                         'networkx >= 1.9.1',
                         'flexx >= 0.4.1',
-                        'bokeh == 0.12.6',
-                        'future'],
-      include_package_data=True,
-      ext_modules=ext_modules,
-      use_2to3=True,
-      cmdclass=cmdclass)
+                        'future']
+else:
+    install_requires = [
+                        'flexx >= 0.4.1',
+                        'future']
+
+version = '0.8.1a6'
+try:
+    setup(name='abce',
+          version=version,
+          author='Davoud Taghawi-Nejad',
+          author_email='Davoud@Taghawi-Nejad.de',
+          description='Agent-Based Complete Economy modelling platform',
+          url='https://github.com/AB-CE/abce.git',
+          package_dir={'abce': 'abce', 'abce.gui': 'abce/gui'},
+          packages=['abce'],
+          long_description=open('README.rst').read(),
+          install_requires=install_requires,
+          include_package_data=True,
+          ext_modules=ext_modules,
+          use_2to3=True,
+          cmdclass=cmdclass)
+    print("ABCE runs with cython")
+except:
+    setup(name='abce',
+          version=version,
+          author='Davoud Taghawi-Nejad',
+          author_email='Davoud@Taghawi-Nejad.de',
+          description='Agent-Based Complete Economy modelling platform',
+          url='https://github.com/AB-CE/abce.git',
+          package_dir={'abce': 'abce', 'abce.gui': 'abce/gui'},
+          packages=['abce'],
+          long_description=open('README.rst').read(),
+          install_requires=install_requires,
+          include_package_data=True,
+          ext_modules=[],
+          use_2to3=True,
+          cmdclass={})
+    print("ABCE RUNS WITHOUT CYTHON, NORMAL ON PYPY")
