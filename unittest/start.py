9d137488a69385936630342c74963d0178a1494f
DavoudTaghawiNejad <davoud@taghawi-nejad.de>
2017-09-05 15:54:38 -0300

    Tests delete_create and logging on multiprocesses under pypy

--- unittest/start.py
+++ unittest/start.py
@@ -7,7 +7,7 @@ if __name__ == '__main__':
     start_logging_test.main(processes=1)
     print('Iteration of logger testing with 1 core finished')
 
-    if (platform.system() != 'Windows' and platform.python_implementation() != 'PyPy'):
+    if (platform.system() != 'Windows'):
         start_logging_test.main(processes=4)
         print('Iteration of logger testing with multiple processes finished')
 
@@ -21,6 +21,8 @@ if __name__ == '__main__':
     start_delete_create.main(processes=1, rounds=30)
     print('Iteration with 1 core finished')
 
-    if (platform.system() != 'Windows' and platform.python_implementation() != 'PyPy'):
+    if (platform.system() != 'Windows'):
         start_delete_create.main(processes=4, rounds=20)
         print('Iteration with multiple processes finished')
+
+    print("PYPY and windows: core functions not tested with multi-processes")
