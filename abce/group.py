96830b40ac34adf2ff8e99a0d7005026b3a2fb64
DavoudTaghawiNejad <davoud@taghawi-nejad.de>
2017-11-11 11:19:54 +0000

    Remove processorgroup, but also multiprocessing and lose the ability to add groups

--- abce/group.py
+++ abce/group.py
@@ -1,3 +1,9 @@
+from collections import deque
+import traceback
+from time import sleep
+import random
+
+
 def get_methods(a_class):
     return [method for method in a_class.__dict__.keys() if
             callable(getattr(a_class, method)) and not
@@ -5,15 +11,10 @@ def get_methods(a_class):
 
 
 class Group(object):
-    def __init__(self, sim, groups, agent_class=None):
+    def __init__(self, sim, group_name, agent_class=None):
         self.sim = sim
         self.num_managers = sim.processes
-        self._processor_groups = sim._processor_groups
-        self.groups = groups
-        self.do = (self.execute_parallel
-                   if sim.processes > 1
-                   else self.execute_serial)
-
+        self.group_name = group_name
         self.agent_class = agent_class
         for method in dir(agent_class):
             if method[0] != '_':
@@ -23,6 +24,7 @@ class Group(object):
 
         self.panel_serial = 0
         self.last_action = "Begin_of_Simulation"
+        self.free_ids = deque()
 
     def __add__(self, g):
         return Group(self.sim, self.groups + g.groups, self.agent_class)
@@ -32,28 +34,6 @@ class Group(object):
             return self.__add__(g)
         else:
             return self
-
-    def execute_serial(self, command, *args, **kwargs):
-        self.last_action = command
-        self.sim.messagess[-1].clear()
-        out_messages = self._processor_groups[0].execute(
-            self.groups, command, [], args, kwargs)
-        self.sim.messagess = out_messages
-        return out_messages[-1]
-
-    def execute_parallel(self, command, *args, **kwargs):
-        self.last_action = command
-        self.sim.messagess[-1].clear()
-        parameters = ((pg, self.groups, command, self.sim.messagess[pgid], args, kwargs)
-                      for pgid, pg in enumerate(
-            self._processor_groups))
-        out = self.sim.pool.map(execute_wrapper, parameters, chunksize=1)
-        for pgid in range(self.num_managers):
-            self.sim.messagess[pgid].clear()
-        for out_messages in out:
-            for pgid, messages in enumerate(out_messages):
-                self.sim.messagess[pgid].extend(messages)
-        return self.sim.messagess[-1]
 
     def panel_log(self, variables=[], possessions=[], func={}, len=[]):
         """ panel_log(.) writes a panel of variables and possessions
@@ -107,7 +87,97 @@ class Group(object):
         """
         self.do('_agg_log', variables, possessions, func, len)
 
+    def add_group(self, Agent, num_agents_this_group, agent_args, parameters,
+                  agent_parameters, agent_params_from_sim):
+        self.agents = []
+        self.apfs = agent_params_from_sim
+        for i in range(num_agents_this_group):
+            agent = self.make_an_agent(Agent, id=i, agent_args=agent_args,
+                                       parameters=parameters,
+                                       agent_parameters=agent_parameters[i])
+            self.agents.append(agent)
 
-def execute_wrapper(inp):
-    # processor_group.execute(self.groups, command, messages[pgid])
-    return inp[0].execute(inp[1], inp[2], inp[3], inp[4], inp[5])
+    def append(self, Agent, agent_args, parameters, agent_parameters):
+        try:
+            id = self.free_ids.popleft()
+        except IndexError:
+            id = len(self.agents)
+            self.agents.append(None)
+        agent = self.make_an_agent(
+            Agent, id, agent_args, parameters, agent_parameters)
+        self.agents[id] = agent
+        return id
+
+    def make_an_agent(self, Agent, id, agent_args,
+                      parameters, agent_parameters):
+        agent_args['num_managers'] = self.num_managers
+        agent = Agent(id=id, **agent_args)
+        for good, duration in self.apfs['expiring']:
+            agent._declare_expiring(good, duration)
+        for good in self.apfs['perishable']:
+            agent._register_perish(good)
+        for resource, units, product in self.apfs['resource_endowment']:
+            agent._register_resource(resource, units, product)
+        try:
+            agent.init(parameters, agent_parameters)
+        except AttributeError:
+            if 'init' not in dir(agent):
+                print("Warning: agent %s has no init function" % agent.group)
+            else:
+                raise
+        except KeyboardInterrupt:
+            return None
+        except Exception:
+            sleep(random.random())
+            traceback.print_exc()
+            raise Exception()
+        return agent
+
+    def do(self, command, *args, **kwargs):
+        self.last_action = command
+        rets = []
+        self.put_messages_in_pigeonbox()
+        for agent in self.agents:
+            try:
+                messages, ret = agent._execute(command, args, kwargs)
+                for group, msg in messages.items():
+                    self.sim.messagess[group].extend(msg)
+                rets.append(ret)
+            except AttributeError:
+                pass
+        return rets
+
+    def delete_agent(self, id):
+        self.agents[id] = None
+        self.free_ids.append(id)
+
+    def name(self):
+        return (self.group, self.batch)
+
+    def execute_advance_round(self, time):
+        for agent in self.agents:
+            try:
+                agent._advance_round(time)
+            except KeyboardInterrupt:
+                return None
+            except AttributeError:
+                pass
+            except Exception:
+                sleep(random.random())
+                traceback.print_exc()
+                raise Exception()
+
+    def put_messages_in_pigeonbox(self):
+        for _, id, msg in self.sim.messagess[self.group_name]:
+            self.agents[id].inbox.append(msg)
+        self.sim.messagess[self.group_name].clear()
+
+    def len(self):
+        """ Returns the length of a group """
+        return sum([1 for agent in self.agents if agent is not None])
+
+    def repr(self):
+        return str(self.batch)
+
+    def __repr__(self):
+        return repr()
