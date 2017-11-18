558cb91f9fc928b3655ef6315e088773c8bf90a6
DavoudTaghawiNejad <davoud@taghawi-nejad.de>
2017-11-18 11:27:17 +0000

    Fix hanging sentence in agent._send

--- abce/agent.py
+++ abce/agent.py
@@ -349,8 +349,8 @@ class Agent(Database, Trade, Messaging):
         self._inventory._perishable.append(good)
 
     def _send(self, receiver_group, receiver_id, typ, msg):
-        """ sends a message to 'receiver_group', who can be an agent, a group
-        The agents receives it at the begin of each round in
+        """ sends a message to 'receiver_group', 'receiver_id'
+        The agents receives it at the begin of each round.
         """
         self._out[receiver_id % self.num_managers].append(
             (receiver_group, receiver_id, (typ, msg)))
