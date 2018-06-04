b6e620f33db1269980a6263e9ff24bfc18f8a421
DavoudTaghawiNejad <davoud@taghawi-nejad.de>
2018-06-04 13:34:09 +0100

    Unpack pluging messages

--- abce/messenger.py
+++ abce/messenger.py
@@ -200,7 +200,7 @@ class Messenger:
                 if msg[0] == 'd':
                     del self._contracts_deliver[msg[1]][msg[2]]
             elif typ == 'forceexecute':
-                getattr(self, msg[0])(msg[1:])
+                getattr(self, msg[0])(*msg[1:])
             else:
                 self._msgs.setdefault(typ, []).append(msg)
         self.inbox.clear()
